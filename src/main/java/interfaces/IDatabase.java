75ef7a302e3e5ec3eade6f469ec35900ecee6aba
Adam Jankowski <adimustbefunny@gmail.com>
2020-02-02 13:40:33 +0100

    finished user class

--- src/main/java/interfaces/IDatabase.java
+++ src/main/java/interfaces/IDatabase.java
@@ -1,4 +1,11 @@
 package interfaces;
 
+import java.util.List;
+
 public interface IDatabase {
+    List<IUser> getUsers();
+    List<IProduct> getProducts();
+
+    void saveUsers(List<IUser> users);
+    void saveProducts(List<IProduct> products);
 }
