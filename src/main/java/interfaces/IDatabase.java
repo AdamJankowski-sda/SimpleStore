fd17febab6dd13af58dfb91c2d76d649a1547758
Adam Jankowski <adimustbefunny@gmail.com>
2020-02-02 13:57:30 +0100

    completed database interface

--- src/main/java/interfaces/IDatabase.java
+++ src/main/java/interfaces/IDatabase.java
@@ -1,4 +1,11 @@
 package interfaces;
 
+import java.util.List;
+
 public interface IDatabase {
+    List<IProduct> getProducts();
+    List<IUser> getUsers();
+
+    void saveProducts(List<IProduct> products);
+    void saveUsers(List<IUser> users);
 }
