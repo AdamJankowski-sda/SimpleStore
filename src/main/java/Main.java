0560caa2005d62e96cb6c6a152ad8f09eac41cc9
Adam Jankowski <adimustbefunny@gmail.com>
2020-02-06 12:23:38 +0100

    updated main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import cartview.CartView;
+import database.LocalDatabase;
+import interfaces.*;
+import storecart.StoreCart;
+import storeservice.StoreService;
+import storeview.StoreView;
+import userdata.UserData;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        IDatabase database = new LocalDatabase();
+        IStoreService storeService = new StoreService(database.getProducts());
+        IUserData userData = new UserData(database.getUsers());
+
+        IStoreCart storeCart = new StoreCart(new ArrayList<>(){{add(storeService.getProducts().get(0));}});
+
+        IStoreView storeView = new StoreView(storeService);
+        ICartView cartView = new CartView(storeCart);
+
+        System.out.println("Sklep");
+        storeView.printProductList();
+
+        System.out.println("Kosz");
+        cartView.printProductList();
 
     }
 }
